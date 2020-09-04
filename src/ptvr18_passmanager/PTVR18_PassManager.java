09c72109a31977340974e032b60e36c617e7284b
pupil <pupil@ivkhk.ee>
2020-09-04 14:24:37 +0300

    Create project

---
+++ src/ptvr18_passmanager/PTVR18_PassManager.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ptvr18_passmanager;
+
+/**
+ *
+ * @author pupil
+ */
+public class PTVR18_PassManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
